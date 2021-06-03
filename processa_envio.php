aa20b88da1fb70cef33da03fd247761a4aa4f4d2
VictorMont <victormonteir2@gmail.com>
2021-06-03 17:48:11 -0300

    Envio de email concluido e layout de pagina de sucesso

--- processa_envio.php
+++ processa_envio.php
@@ -58,7 +58,7 @@
     //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = '';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'monteir2victor@gmail.com';                     //SMTP username
         $mail->Password   = 'dbz@2030';                               //SMTP password
