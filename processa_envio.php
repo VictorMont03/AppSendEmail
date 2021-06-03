c450cbd043ed09d8a1c0f961635e2a941dd13c81
VictorMont <victormonteir2@gmail.com>
2021-06-03 18:00:36 -0300

    Pequenas atualizações para possiveis erros

--- processa_envio.php
+++ processa_envio.php
@@ -60,8 +60,8 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'monteir2victor@gmail.com';                     //SMTP username
-        $mail->Password   = 'dbz@2030';                               //SMTP password
+        $mail->Username   = 'monkeytheluffy290@gmail.com';                     //SMTP username
+        $mail->Password   = 'carne@carne';                               //SMTP password
         $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
